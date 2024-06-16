378a808c55b8a03eb258c00e3e864e1ac1561f00
Adesodun Titilope Temiloluwa <115459326+adesodun27@users.noreply.github.com>
2024-06-16 16:33:37 +0100

    Add Vercel configuration and fix routing issues

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
-    build: {
-      outDir: 'dist',
+  plugins: [
+    react(),
+    // Add other plugins as needed
+  ],
+  build: {
+    outDir: "dist", // Specify the output directory for production builds
+    sourcemap: true, // Generate source maps for debugging
+  },
+  resolve: {
+    alias: {
+      // Example alias for src directory
+      "@": path.resolve(__dirname, "src"),
     },
+  },
   esbuild: {
     loader: "jsx",
-    include: [
-      // Add this
-      "src/**/*.js",
-      "src/**/*.jsx",
-    ],
+    include: ["src/**/*.js", "src/**/*.jsx"],
     exclude: [],
   },
 });
